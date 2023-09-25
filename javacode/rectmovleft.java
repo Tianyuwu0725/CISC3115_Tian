2fa3103988c755ebe6e8274bf2f80f7ffa9bc174
Tianyuwu0725 <Tianyu.Wu@mail.citytech.cuny.edu>
2023-09-24 22:21:04 -0400

    message

---
+++ javacode/rectmovleft.java
@@ -0,0 +1,27 @@
+import java.awt.Point;
+import java.awt.Rectangle;
+
+public class rectmovleft {
+	public static Rectangle rectmovleft(Rectangle box, Point p) throws IllegalArgumentException {
+		if(p.x<0) {
+			throw new IllegalArgumentExeption("Usage: input x is not legal");
+		}
+		box.x=box.x+p.x;
+		box.y=box.y+p.y;
+
+		return box;
+	}
+
+	public static void main(String[] args) {
+		Rectangle box=new Rectaangle(0,0,100,200);
+		Point p=new Point(3,4);
+
+		try{
+			Rectangle result=rectmovleft(box,p);
+			System.out.println("Reuslting Rectangle: " + result);
+		} catch(IllegalArgumentExxception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
+
